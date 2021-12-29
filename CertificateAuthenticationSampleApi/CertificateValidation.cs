3cd76f0996a3c4164dd9efe8dd178439407b6812
Vikash Tripathi <tripvik@gmail.com>
2021-12-29 11:22:41 -0500

    Add project files.

---
+++ CertificateAuthenticationSampleApi/CertificateValidation.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CertificateAuthenticationSampleApi
+{
+    internal class CertificateValidation
+    {
+        public bool ValidateCertificate(X509Certificate2 clientCertificate)
+        {
+            string[] allowedThumbprints = {
+                "13874323536A3BEFFFA4BB2FE97BFAFB8357240A"
+            };
+            System.Console.WriteLine(clientCertificate.Thumbprint);
+            if (allowedThumbprints.Contains(clientCertificate.Thumbprint))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ CertificateAuthenticationSampleClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CertificateAuthenticationSampleClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await SendRequest();
+            Console.ReadKey();
+        }
+
+        static async Task SendRequest()
+        {
+
+            try
+            {
+                var cert = new X509Certificate2(Path.Combine(Environment.CurrentDirectory, "client.pfx"), "test");
+                var handler = new HttpClientHandler();
+                handler.ClientCertificates.Add(cert);
+                var client = new HttpClient(handler);
+
+                var request = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri("https://localhost:5001/WeatherForecast"),
+                    Method = HttpMethod.Get,
+                };
+
+                var response = await client.SendAsync(request);
+
+                Console.WriteLine(response.StatusCode);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseContent);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
